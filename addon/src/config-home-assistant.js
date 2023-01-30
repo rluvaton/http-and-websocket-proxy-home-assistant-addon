ad54bcafef1fea38fd4b99e8fb777a60ef9ab45e
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-01-30 20:56:00 +0200

    set default values in config

--- addon/src/config-home-assistant.js
+++ addon/src/config-home-assistant.js
@@ -14,17 +14,25 @@ let config;
 async function loadConfig() {
   if (!config) {
     try {
-      const configStr = (await fs.readFile(CONFIG_FILE)).toString();
-
-      config = deepFreeze(JSON.parse(configStr));
+      config = JSON.parse((await fs.readFile(CONFIG_FILE)).toString());
     } catch (error) {
       logger.error({ path: CONFIG_FILE, error }, 'Could not parse options file');
 
       throw error;
     }
+
+    config = deepFreeze(setDefaults(config));
   }
 
   return config;
+}
+
+function setDefaults(configObj) {
+  configObj.localHomeAssistantUrl = configObj.localHomeAssistantUrl ?? process.env.LOCAL_HOME_ASSISTANT_URL ?? 'http://homeassistant.local:8123'
+  configObj.logLevel = configObj.logLevel ?? 'info';
+  configObj.remoteWsUrl = configObj.remoteWsUrl ?? process.env.REMOTE_WS_URL ?? 'ws://localhost:3000';
+
+  return configObj;
 }
 
 
