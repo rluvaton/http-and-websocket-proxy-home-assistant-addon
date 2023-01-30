850802c8a12df4368e8a7670a53006e3e18b34f2
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2023-01-30 19:12:52 +0200

    fix 500 timeout error happened due to content length was in the headers

--- addon/src/index.js
+++ addon/src/index.js
@@ -222,6 +222,12 @@ function sendMessages(req) {
 }
 
 function proxyHttpRequest(req) {
+  const contentLengthHeaders = Object.keys(req.headers).filter(header => header.toLowerCase() === 'content-length');
+
+  // Deleting the content length, as it should be recalculated, otherwise the request would time out after a while
+  contentLengthHeaders.forEach((header) => {
+    delete req.headers[header];
+  });
   return axios.request({
     method: req.method,
     data: req.body,
